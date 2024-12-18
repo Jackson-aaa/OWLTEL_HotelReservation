81064bf2a11ad340f573149e94bbd0998b8f1736
Jackson Ang <jackson.ang@binus.edu>
2024-12-18 11:26:44 +0700

    add booking history route

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -13,20 +13,20 @@
         @endif
     </div>
     <div>
-        
-    @if (!(Auth::check() && Auth::user()->type === 'admin'))
-        <form class="search-form">
-        @csrf
-            <input type="text" placeholder="Search destination" class="search-input" name="destination" required>
-            <div></div>
-            <input type="datetime" placeholder="Check In" name="checkin" required>
-            <div></div>
-            <input type="datetime" placeholder="Check Out" name="checkout" required>
-            <button type="submit" class="search-button">
-                <i class="fa-solid fa-search"></i>
-            </button>
-        </form>
-    @endif
+
+        @if (!(Auth::check() && Auth::user()->type === 'admin'))
+            <form class="search-form">
+                @csrf
+                <input type="text" placeholder="Search destination" class="search-input" name="destination" required>
+                <div></div>
+                <input type="datetime" placeholder="Check In" name="checkin" required>
+                <div></div>
+                <input type="datetime" placeholder="Check Out" name="checkout" required>
+                <button type="submit" class="search-button">
+                    <i class="fa-solid fa-search"></i>
+                </button>
+            </form>
+        @endif
 
     </div>
     <div class="auth-button">
@@ -38,10 +38,28 @@
             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                 @csrf
             </form>
-            <a href="{{ route('logout') }}" id="sign-out"
-                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                Sign Out
-            </a>
+            @if (!(Auth::check() && Auth::user()->type !== 'admin'))
+
+                <a href="{{ route('logout') }}" id="sign-out"
+                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                    Sign Out
+                </a>
+            @else
+                <div class="btn-group">
+                    <a data-bs-toggle="dropdown" href="" type="button">
+                        Profile
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li class="p-1"><a href="{{route('bookinghistory')}}">Booking History</a></li>
+                        <li class="p-1">
+                            <a href="{{ route('logout') }}" id="sign-out"
+                                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                Sign Out
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            @endif
         @endauth
     </div>
 </nav>
